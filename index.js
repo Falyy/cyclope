bc310ae63de2dab2f8159bb5da10e03f2c247ab5
Falyy <falou89@gmail.com>
2019-02-16 20:59:23 +0100

    change verify_token

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
+  const VERIFY_TOKEN = "<Cyclope_Jade_Project>";
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
